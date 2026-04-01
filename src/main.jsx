9ecb437fa4d77ff3fd2bd0d163c64cd04b2b9f1e
gabepost95 <gabepost95@gmail.com>
2026-04-01 14:49:48 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DocForce from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DocForce />
+  </React.StrictMode>
+)
